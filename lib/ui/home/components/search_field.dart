87b830f39ca102192364731d648f9938faf3d034
shizopudge <91852426+shizopudge@users.noreply.github.com>
2023-03-26 11:49:00 +0500

    seventh commit

--- lib/ui/home/components/search_field.dart
+++ lib/ui/home/components/search_field.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../bloc/images_bloc/images_bloc.dart';
+import '../../../core/colors.dart';
 import '../../../core/fonts.dart';
 
 class SearchField extends StatelessWidget {
@@ -20,18 +21,24 @@ class SearchField extends StatelessWidget {
       color: Colors.white,
       child: TextField(
         controller: searchController,
-        onSubmitted: (query) => context.read<ImagesBloc>().add(
-              ImagesEvent.searchImages(
-                page: 1,
-                query: query,
-              ),
-            ),
-        onEditingComplete: () => context.read<ImagesBloc>().add(
-              ImagesEvent.searchImages(
-                query: searchController.text.trim(),
-                page: 1,
-              ),
-            ),
+        onSubmitted: (query) {
+          context.read<ImagesBloc>().add(
+                ImagesEvent.searchImages(
+                  page: 1,
+                  query: query,
+                ),
+              );
+          FocusScopeNode currentFocus = FocusScope.of(context);
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        },
+        // onEditingComplete: () => context.read<ImagesBloc>().add(
+        //       ImagesEvent.searchImages(
+        //         query: searchController.text.trim(),
+        //         page: 1,
+        //       ),
+        //     ),
         cursorColor: Colors.black,
         cursorWidth: 1,
         cursorRadius: const Radius.circular(21),
@@ -45,29 +52,62 @@ class SearchField extends StatelessWidget {
           hintStyle: AppFonts.smallStyle.copyWith(
             color: Colors.grey,
           ),
-          prefixIcon: Icon(
-            Icons.search,
-            color: Colors.grey.shade700,
+          prefixIcon: ValueListenableBuilder(
+            valueListenable: searchController,
+            builder: (context, value, child) => searchController.text.isEmpty
+                ? Icon(
+                    Icons.search,
+                    color: Colors.grey.shade700,
+                  )
+                : InkWell(
+                    borderRadius: BorderRadius.circular(21),
+                    onTap: () {
+                      context.read<ImagesBloc>().add(
+                            ImagesEvent.searchImages(
+                              page: 1,
+                              query: searchController.text.trim(),
+                            ),
+                          );
+                      FocusScopeNode currentFocus = FocusScope.of(context);
+                      if (!currentFocus.hasPrimaryFocus) {
+                        currentFocus.unfocus();
+                      }
+                    },
+                    child: ShaderMask(
+                      shaderCallback: (bounds) =>
+                          AppColors.linearGradientOrange,
+                      blendMode: BlendMode.srcIn,
+                      child: const Icon(
+                        Icons.search,
+                      ),
+                    ),
+                  ),
           ),
           suffixIcon: ValueListenableBuilder(
             valueListenable: searchController,
             builder: (context, value, child) {
-              return searchController.text.isNotEmpty
-                  ? IconButton(
-                      onPressed: () {
-                        searchController.clear();
-                        context.read<ImagesBloc>().add(
-                              const ImagesEvent.getImages(
-                                page: 1,
-                              ),
-                            );
-                      },
-                      icon: Icon(
-                        Icons.clear_rounded,
-                        color: Colors.grey.shade900,
-                      ),
-                    )
-                  : const SizedBox();
+              if (searchController.text.isNotEmpty) {
+                return IconButton(
+                  onPressed: () {
+                    searchController.clear();
+                    context.read<ImagesBloc>().add(
+                          const ImagesEvent.getImages(
+                            page: 1,
+                          ),
+                        );
+                  },
+                  icon: ShaderMask(
+                    shaderCallback: (bounds) => AppColors.linearGradientOrange,
+                    blendMode: BlendMode.srcIn,
+                    child: Icon(
+                      Icons.clear_rounded,
+                      color: Colors.grey.shade900,
+                    ),
+                  ),
+                );
+              } else {
+                return const SizedBox();
+              }
             },
           ),
         ),
